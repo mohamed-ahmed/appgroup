8f9c897beb52da12171bd9f7b1f8d0138a2653c0
Your Name <luc785@hotmail.com>
2014-02-15 12:21:10 -0500

    added all points and surrounding points

---
+++ allpoints/Mackeznie_Area.js
@@ -0,0 +1,156 @@
+
+//Path point on Library Road. Near Canal (NC means Near Canal Building)
+var NC_LIBR=new Object({
+lat: 45.384115,
+lng: -75.698906,
+name: "Library Road Near Canal Building",
+building:"null",
+});
+
+// Path point on library road near Lanark House (NLH means Near Lanark House
+var NLH_LIBR=new Object({
+lat: 45.385245,
+lng: -75.698681,
+name: "Library Road near Lanark House",
+building"null",
+});
+
+// Path point on library road near Prescott House (NPH means Near Prescott House
+var NPH_LIBR=new Object({
+lat: 45.385528,
+lng: -75.696385,
+name: "Library Road near Prescott House",
+building:"null",
+});
+//Path Point on the intersection between library road and campus ave (INTR= intersection, LIBR= Library,CAMPA=campus ave 
+var INTER_LIBR_CAMPA=new Object({
+lat: 45.385535;
+lng: -75.696374;
+name: "Library Road Campus Ave Intersection";
+building:"null";
+});
+//Path point between Frontenac(FR) and Renfrew (RH)(BETW=between)
+var PATH_BETW_FR_RH=new Object({
+lat: 45.386048,
+lng: -75.697764,
+name: "Between Frontenac and Renfrew",
+building:"null",
+});
+//Glengarry House Entrance (ENT=Entrance)
+var ENT_GH_W=new Object({
+lat: 45.386647,
+lng: -75.697683,
+name: "Glengarry House Entrance",
+building:"GH",
+});
+// Path point on library road near MintoCenter (NMC means Minto Center
+var NMC_LIBR=new Object({
+lat: 45.384676,
+lng: -75.69638,
+name: "NMC_LIBR",
+building:"null",
+});
+//Path point between CANAL (CB) and Architecture(AA)
+var BETW_CB_AA=new Object({
+lat: 45.384047,
+lng: -75.698021,
+name: "BETW_CB_AA",
+building:"null",
+});
+//East Entrance of Canal
+var ENT_CB_E=new Object({
+lat: 45.384183,
+lng: -75.698354,
+name: "ENT_CB_E",
+building:"CB",
+});
+//West Entrance of Canal
+var ENT_CB_W=new Object({
+lat: 45.384326;
+lng: -75.69845;
+name: "ENT_CB_W";
+building:"CB";
+});
+//South Entrance of building 4 Mackenzie
+var ENT_ME4_S=new Object({
+lat: 45.384462,
+lng: -75.698102,
+name: "ENT_ME4_W",
+building:"ME",
+});
+
+//North Entrance of building 4 Mackenzie
+var ENT_ME4_N=new Object({
+lat: 45.384518,
+lng: -75.697984,
+name: "ENT_ME4_E",
+building:"ME",
+});
+
+//East Entrance of building 3 Mackenzie
+var ENT_ME3_E=new Object({
+lat: 45.384793,
+lng: -75.697994,
+name: "ENT_ME3_E",
+building:"ME",
+});
+
+//West Entrance of building 3 Mackenzie
+var ENT_ME3_W=new Object({
+lat: 45.38488,
+lng: -75.698096,
+name: "ENT_ME3_W",
+building:"ME",
+});
+//South Entrance of building 2 Mackenzie
+var ENT_ME2_S=new Object({
+lat: 45.384853,
+lng: -75.69771,
+name: "ENT_ME2_S",
+building:"ME",
+});
+//North Entrance of building 2 Mackenzie
+var ENT_ME2_N=new Object({
+lat: 45.384936,
+lng: -75.69763,
+name: "ENT_ME2_M",
+building:"ME",
+});
+
+//NorthEast Entrance of building 2 Mackenzie
+var ENT_ME2_NE=new Object({
+lat: 45.385185,
+lng: -75.697801,
+name: "ENT_ME2_NE",
+building:"ME",
+});
+
+//NorthEast Entrance of building 3 Mackenzie
+var ENT_ME3_SW=new Object({
+lat: 45.38448,
+lng: -75.698364,
+name: "ENT_ME3_SW",
+building:"ME",
+});
+
+//NorthEast Entrance of building 3 Mackenzie
+var ENT_ME1_NE=new Object({
+lat: 45.384733,
+lng: -75.697168,
+name: "ENT_ME1_NE",
+building:"ME",
+});
+
+var MC_ENT_N=new Object({
+lat: 45.385249,
+lng: -75.696817,
+name: "MC_ENT_N",
+building:"MC",
+});
+var MC_ENT_NW=new Object({
+lat: 45.385189,
+lng: -75.6973,
+name: "MC_ENT_NW",
+building:"MC",
+});
+
---
+++ allpoints/testing/DijkstraAlgorithim.js
@@ -0,0 +1,190 @@
+/**************************************************************
+ *                                                            *
+ *   Dijkstria's Algorithim is a graph searching algorithim   *
+ *   that finds the shortest path, if it exists, between      *
+ *   two points by finding the shortest distance between      *
+ *   those two points and any connecting points inbetween.    *
+ *                                                            *
+ *   @param graph  dictionary object representing the map     *
+ *   @param start_point  Node object of the starting pos      *
+ *   @param end_point   Node object of the destination        *
+ *                                                            *
+ *   @return path  array of Node objects that represent the   *
+ *								 shortest path between start_point and      *
+ * 								 end_point. If the graph has no link,				*
+ *								 then the function returns null.            *
+ *                                                            *
+ *                                                            *
+ *   Author: Jeton Sinoimeri																	*
+ *   Created: February 12, 2014                               *
+ *   Last Modified: February 13, 2014													*
+ *                                                            *
+ *																														*
+ *	 Sources:																									*
+ *   1) http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm   *
+ *   2) Dijkstra_Algorithim.py																*
+ *				Author: Jeton Sinoimeri															*
+ *				Created: May 2011																		*
+ *        Last Modified: June 2011														*
+ *																														*
+ *																														*
+ **************************************************************/
+
+
+function dijkstra_algorithim(graph, start_point, end_point) 
+{
+	var INF = Number.MAX_VALUE;									// max value
+	var NOT_DEFINED = Number.NaN;								// not defined
+	
+	var dist_min = Number.POSITIVE_INFINITY; 		// min distance is infinity
+	
+	var distance = { };										
+	var prev = { };												
+	
+	var Q = [ ];				// copy of graph
+	var paths = [];			// most optimal path
+	 
+	
+	// for each vertex makes the distance and previous connection 
+	// infitity and not defined respectfully
+	
+	for (var vertex in graph)
+	{
+			distance[vertex] = INF;
+      prev[vertex] = NOT_DEFINED;
+      Q.push(vertex);
+	}
+	
+	
+	// distance from source to source
+	distance[source] = 0;
+	
+	
+	while (Q.length > 0)
+	{
+		
+		for (vertex in Q)
+		{
+			if (distance[vertex] < dist_min)
+			{
+				dist_min = distance[vertex];
+				vertex_min = vertex;
+			}
+		}
+            
+            
+        
+		// returns null if graph has no link
+		if (dist_min == INF) 
+		{
+			return null;
+		}
+		
+		
+		// finds the shortest path between start point and end point
+		if (vertex_min == end_point)
+		{
+			 var vrtx_path = end_point;
+            
+       while (prev[vrtx_path] != NOT_DEFINED)
+       {
+            paths.unshift(vrtx_path);   		// adds vrtx_path to front of array
+            vrtx_path = prev[vrtx_path];		// gets the next node in prev
+       }         
+       
+       paths.unshift(source);  // adds source to front of array
+            
+       return paths;					// returns the shortest path
+		}
+		
+		
+		// if the vertex_min is not equal to the end point then it finds the 
+    // next vertex_min
+    
+		else
+		{
+			// deletes vertex_min from the copy array
+			for (var i = 0; i < Q.length; i++)
+			{
+				if (Q[i] == vertex_min)
+				{
+					delete Q[i];
+				}
+			}
+			
+			// finds each neighbouring node of vertex_min in the graph
+			for (neighbour in vertex_min.surr)
+			{
+				 // adds the distances together if the neighbour is in Q
+				 if (neighbour in Q)
+         {
+         		var alt = distance[vertex_min] + find_distance(vertex_min, neighbour); 
+         }
+         
+                
+         if (alt < distance[neighbour])
+         {
+         		distance[neighbour] = alt;	 		// assigns the value alt to neighbour key in distance
+            prev[neighbour] = vertex_min;		// assigns vertex_min as a value to neighbour key in prev
+         }
+                    
+			}
+            
+		}
+		
+	}
+	
+}
+
+
+
+
+/**************************************************************
+ *                                                            *
+ *   Finds the distance between two Nodes using their gps     *
+ *   coordinates using Haversine formula.                     *
+ *                                                            *
+ *   @param vertex_min  current Node object										*
+ *   @param neighbour   neighbouring Node object              *
+ *                                                            *
+ *   @return distance   double precision floating point       *
+ *											representing the distance between     *
+ *											the two Nodes													*
+ *                                                            *
+ *                                                            *
+ *   Author: Jeton Sinoimeri																	*
+ *   Created: February 13, 2014                               *
+ *   Last Modified: February 13, 2014													*
+ *																														*
+ *																														*
+ *	 Source: 																									*
+ *			http://www.movable-type.co.uk/scripts/latlong.html		*
+ *                                                            *
+ **************************************************************/
+
+
+function find_distance(vertex_min, neighbour)
+{
+	var RADIUS = 6371; // radius of Earth in km
+	
+	
+	// finds the difference in latitude and longitude and converts them to radian
+	var delta_lat = (neighbour.lat - vertex_min.lat).toRad();
+	var delta_lon = (neighbour.lon - vertex_min.lon).toRad();
+	
+	
+	// converts the latitudes of vertex_min and neighbour to radian
+	var vrtx_lat_rad = vertex_min.lat.toRad();
+	var neigh_lat_rad = neighbour.lat.toRad();
+
+
+	// Haversine formula
+	var half_chord_length = Math.sin(delta_lat / 2) * Math.sin(delta_lat / 2) + Math.sin(delta_lon / 2) * Math.sin(delta_lon / 2) * Math.cos(neigh_lat_rad) * Math.cos(vrtx_lat_rad);       
+	var angular_distance = 2 * Math.atan2(Math.sqrt(half_chord_length), Math.sqrt(1 - half_chord_length)); 
+	
+	
+	// the distance between two nodes in km
+	var distance = RADIUS * angular_distance;
+	
+	return distance;
+}
