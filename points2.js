29c7df1c722b06553f90f3700f47250be95de051
IMaz6 <ihtisham-6@hotmail.com>
2014-02-26 01:38:30 -0600

    Points2.js - Second set of points (B),A&B are from Testpoints.zip

---
+++ points2.js
@@ -0,0 +1,864 @@
+//
+var B0 = new Object({
+			lat: 45.385185,
+			lng: -75.698338,
+			name: "", 
+			building: "null"
+		});
+//
+var B1 = new Object({
+			lat: 45.385049,
+			lng:-75.698348,
+			name: "B1", 
+			building: "null"
+		});
+//
+var B2 = new Object({
+			lat: 45.385283,
+			lng: -75.697737,
+			name: "B2", 
+			building: "null"
+		});
+//
+var B3 = new Object({
+			lat: 45.384921,
+			lng: -75.696385,
+			name: "B3", 
+			building: "null"
+		});
+//
+var B4 = new Object({
+			lat: 45.385042,
+			lng: -75.697469,
+			name: "B4", 
+			building: "null"
+		});
+//
+var B5 = new Object({
+			lat: 45.385321,
+			lng: -75.697308,
+			name: "B5", 
+			building: "null"
+		});
+//
+var B6 = new Object({
+			lat: 45.384688,
+			lng: -75.69882,
+			name: "B6", 
+			building: "null"
+		});
+//
+var B6a = new Object({
+			lat: 45.384326,
+			lng: -75.698719,
+			name: "B6a", 
+			building: "null"
+		});
+//
+var B7 = new Object({
+			lat: 45.384831,
+			lng: -75.698107,
+			name: "B7", 
+			building: "ME"
+		});
+//
+var B7a = new Object({
+			lat: 45.38517,
+			lng: -75.69779,
+			name: "B7a", 
+			building: "ME"
+		});
+//
+var B7b = new Object({
+			lat: 45.384884,
+			lng: -75.697662,
+			name: "B7b", 
+			building: "ME"
+		});
+//
+var B7c = new Object({
+			lat: 45.384805,
+			lng: -75.697136,
+			name: "B7c", 
+			building: "ME"
+		});
+//
+var B7d = new Object({
+			lat: 45.384496,
+			lng: -75.698397,
+			name: "B7d", 
+			building: "ME"
+		});
+//
+var B7e = new Object({
+			lat: 45.384496,
+			lng: -75.698032,
+			name: "B7e", 
+			building: "ME"
+		});
+//
+var B7f = new Object({
+			lat: 45.384292,
+			lng: -75.69763,
+			name: "B7f", 
+			building: "ME"
+		});
+//
+var B8 = new Object({
+			lat: 45.384432,
+			lng: -75.698327,
+			name: "B8", 
+			building: "null"
+		});
+//
+var B9 = new Object({
+			lat: 45.384733,
+			lng: -75.698021,
+			name: "B9", 
+			building: "null"
+		});
+//
+var B9a = new Object({
+			lat: 45.384642,
+			lng: -75.697624,
+			name: "B9a", 
+			building: "null"
+		});
+//
+var B10 = new Object({
+			lat: 45.384432,
+			lng: -75.697436,
+			name: "B10", 
+			building: "null"
+		});
+//
+var B11 = new Object({
+			lat: 45.384857,
+			lng: -75.697056,
+			name: "B11", 
+			building: "null"
+		});
+//
+var B12 = new Object({
+			lat: 45.384714,
+			lng: -75.696932,
+			name: "B12", 
+			building: "null"
+		});
+//
+var B13 = new Object({
+			lat: 45.384918,
+			lng:-75.696766,
+			name: "B13", 
+			building: "MC"
+		});
+//
+var B13a = new Object({
+			lat: 45.385358,
+			lng:-75.696755,
+			name: "B13a", 
+			building: "MC"
+		});
+//
+var B14 = new Object({
+			lat: 45.385358,
+			lng:-75.696755,
+			name: "B14", 
+			building: "CB"
+		});
+//
+var B14a = new Object({
+			lat: 45.383994,
+			lng:-75.698466,
+			name: "B14a", 
+			building: "CB"
+		});
+//
+var B14b = new Object({
+			lat: 45.385358,
+			lng:-75.696755,
+			name: "B14b", 
+			building: "CB"
+		});
+//
+var B15 = new Object({
+			lat: 45.38413,
+			lng:-75.697994,
+			name: "B15", 
+			building: "null"
+		});
+//
+var B16 = new Object({
+			lat: 45.383761,
+			lng:-75.698606,
+			name: "B16", 
+			building: "null"
+		});
+//
+var B17 = new Object({
+			lat: 45.384138,
+			lng:-75.696954,
+			name: "B17", 
+			building: "null"
+		});
+//
+var B18 = new Object({
+			lat: 45.383527,
+			lng:-75.698896,
+			name: "B18", 
+			building: "null"
+		});
+//
+var B19 = new Object({
+			lat: 45.383655,
+			lng: -75.697737,
+			name: "B19", 
+			building: "null"
+		});
+//
+var B20 = new Object({
+			lat: 45.383648,
+			lng: -75.698434,
+			name: "B20", 
+			building: "UC"
+		});
+//
+var B20a = new Object({
+			lat: 45.38358,
+			lng: -75.697361,
+			name: "B20a", 
+			building: "UC"
+		});
+//
+var B20b = new Object({
+			lat: 45.383037,
+			lng: -75.697833,
+			name: "B20b", 
+			building: "UC"
+		});
+//
+var B21 = new Object({
+			lat: 45.383045,
+			lng: -75.697286,
+			name: "B21", 
+			building: "null"
+		});
+//
+var B21 = new Object({
+			lat: 45.383045,
+			lng: -75.697286,
+			name: "B21", 
+			building: "null"
+		});
+//
+var B22 = new Object({
+			lat: 45.383241,
+			lng: -75.698069,
+			name: "B22", 
+			building: "null"
+		});
+//
+var B23 = new Object({
+			lat: 45.382985,
+			lng: -75.698273,
+			name: "B23", 
+			building: "null"
+		});
+//
+var B24 = new Object({
+			lat: 45.383286,
+			lng: -75.699132,
+			name: "B24", 
+			building: "null"
+		});
+//
+var B25 = new Object({
+			lat: 45.382766,
+			lng: -75.698627,
+			name: "B25", 
+			building: "null"
+		});
+//
+var B26x = new Object({
+			lat: 45.383135,
+			lng: -75.698853,
+			name: "B26x", 
+			building: "AT"
+		});
+//
+var B26xa = new Object({
+			lat: 45.383392,
+			lng: -75.698541,
+			name: "B26xa", 
+			building: "AT"
+		});
+//
+var B26 = new Object({
+			lat: 45.382608,
+			lng: -75.698348,
+			name: "B26", 
+			building: "TB"
+		});
+//
+var B26a = new Object({
+			lat: 45.382578,
+			lng: -75.69779,
+			name: "B26a", 
+			building: "TB"
+		});
+//
+var B27 = new Object({
+			lat: 45.383279,
+			lng: -75.696964,
+			name: "B27", 
+			building: "null"
+		});
+//
+var B28 = new Object({
+			lat: 45.382909,
+			lng: -75.697737,
+			name: "B28", 
+			building: "null"
+		});
+//
+var B29 = new Object({
+			lat: 45.382623,
+			lng: -75.697565,
+			name: "B29", 
+			building: "null"
+		});
+//
+var B30 = new Object({
+			lat: 45.384002,
+			lng: -75.697833,
+			name: "B30", 
+			building: "AA"
+		});
+//
+var B30a = new Object({
+			lat: 45.38404,
+			lng: -75.697082,
+			name: "B30a", 
+			building: "AA"
+		});
+//
+var B31 = new Object({
+			lat: 45.382623,
+			lng: -75.697565,
+			name: "B31", 
+			building: "null"
+		});
+//
+var B32 = new Object({
+			lat: 45.382796,
+			lng: -75.698756,
+			name: "B32", 
+			building: "AP"
+		});
+//
+var B33 = new Object({
+			lat: 45.382548,
+			lng: -75.699185,
+			name: "B33", 
+			building: "DT"
+		});
+//
+var B34 = new Object({
+			lat: 45.382548,
+			lng: -75.698885,
+			name: "B34", 
+			building: "DT"
+		});
+//
+var B35 = new Object({
+			lat: 45.382133,
+			lng: -75.699357,
+			name: "B35", 
+			building: "ML"
+		});
+//
+var B35a = new Object({
+			lat: 45.381998,
+			lng: -75.699883,
+			name: "B35a", 
+			building: "ML"
+		});
+//
+var B35b = new Object({
+			lat: 45.381658,
+			lng: -75.699636,
+			name: "B35b", 
+			building: "ML"
+		});
+//
+var B36 = new Object({
+			lat: 45.382299,
+			lng: -75.699303,
+			name: "B36", 
+			building: "null"
+		});
+//
+var B37 = new Object({
+			lat:45.382344,
+			lng: -75.69822,
+			name: "B37", 
+			building: "null"
+		});
+//
+var B38 = new Object({
+			lat:45.381967,
+			lng: -75.698949,
+			name: "B38", 
+			building: "null"
+		});
+//
+var B39 = new Object({
+			lat: 45.38196,
+			lng: -75.698617,
+			name: "B39", 
+			building: "PA"
+		});
+//
+var B39a = new Object({
+			lat: 45.381892,
+			lng: -75.698381,
+			name: "B39a", 
+			building: "PA"
+		});
+//
+var B39a = new Object({
+			lat: 45.381892,
+			lng: -75.698381,
+			name: "B39a", 
+			building: "PA"
+		});
+//
+var B40 = new Object({
+			lat: 45.382826,
+			lng: -75.696578,
+			name: "B40", 
+			building: "SC"
+		});
+//
+var B40a = new Object({
+			lat: 45.382517,
+			lng: -75.696857,
+			name: "B40a", 
+			building: "SC"
+		});
+//
+var B41 = new Object({
+			lat: 45.382811,
+			lng: -75.696203,
+			name: "B41", 
+			building: "null"
+		});
+//
+var B42 = new Object({
+			lat: 45.382389,
+			lng: -75.697157,
+			name: "B42", 
+			building: "null"
+		});
+//
+var B43 = new Object({
+			lat: 45.382269,
+			lng: -75.697254,
+			name: "B43", 
+			building: "HP"
+		});
+//
+var B43a = new Object({
+			lat: 45.382126,
+			lng: -75.697308,
+			name: "B43a", 
+			building: "HP"
+		});
+
+//
+var B43b = new Object({
+			lat: 45.382035,
+			lng: -75.697844,
+			name: "B43b", 
+			building: "HP"
+		});
+
+//
+var B43c = new Object({
+			lat: 45.381764,
+			lng: -75.697522,
+			name: "B43c", 
+			building: "HP"
+		});
+//
+var B44 = new Object({
+			lat: 45.382525,
+			lng:-75.696632,
+			name: "B44", 
+			building: "RB"
+		});
+//
+var B44a = new Object({
+			lat: 45.382352,
+			lng: -75.69661,
+			name: "B44a", 
+			building: "RB"
+		});
+//
+var B44b = new Object({
+			lat: 45.3826,
+			lng: -75.696138,
+			name: "B44b", 
+			building: "RB"
+		});
+//
+var B45 = new Object({
+			lat: 45.38251,
+			lng: -75.697683,
+			name: "B45", 
+			building: "null"
+		});
+//
+var B46 = new Object({
+			lat: 45.382329,
+			lng: -75.697726,
+			name: "B46", 
+			building: "null"
+		});
+//
+var B47 = new Object({
+			lat: 45.382073,
+			lng: -75.698005,
+			name: "B47", 
+			building: "null"
+		});
+//
+var B48 = new Object({
+			lat: 45.381651,
+			lng: -75.698048,
+			name: "B48", 
+			building: "null"
+		});
+//
+var B49 = new Object({
+			lat: 45.381417,
+			lng: -75.698059,
+			name: "B49", 
+			building: "null"
+		});
+//
+var B50 = new Object({
+			lat: 45.381282,
+			lng: -75.698423,
+			name: "B50", 
+			building: "null"
+		});
+//
+var B51 = new Object({
+			lat: 45.381779,
+			lng: -75.696986,
+			name: "B51", 
+			building: "null"
+		});
+//
+var B52 = new Object({
+			lat: 45.382156,
+			lng: -75.696535,
+			name: "B52", 
+			building: "null"
+		});
+//
+var B53 = new Object({
+			lat: 45.382427,
+			lng: -75.696793,
+			name: "B53", 
+			building: "null"
+		});
+//
+var B54 = new Object({
+			lat: 45.382359,
+			lng: -75.699883,
+			name: "B54", 
+			building: "null"
+		});
+//
+var B55 = new Object({
+			lat: 45.381553,
+			lng: -75.700505,
+			name: "B55", 
+			building: "null"
+		});
+//
+var B56 = new Object({
+			lat: 45.381319,
+			lng: -75.700194,
+			name: "B56", 
+			building: "null"
+		});
+//
+var B57 = new Object({
+			lat: 45.381598,
+			lng: -75.69955,
+			name: "B57", 
+			building: "null"
+		});
+//
+var B58 = new Object({
+			lat: 45.381402,
+			lng: -75.698928,
+			name: "B58", 
+			building: "null"
+		});
+//
+var B59 = new Object({
+			lat: 45.38141,
+			lng: -75.699432,
+			name: "B59", 
+			building: "SA"
+		});
+//
+var B59a = new Object({
+			lat: 45.381327,
+			lng: -75.699174,
+			name: "B59a", 
+			building: "SA"
+		});
+//
+var B59b = new Object({
+			lat: 45.381402,
+			lng: -75.699775,
+			name: "B59b", 
+			building: "SA"
+		});
+//
+var B60 = new Object({
+			lat: 45.381108,
+			lng: -75.699174,
+			name: "B60", 
+			building: "null"
+		});
+//
+var B61 = new Object({
+			lat: 45.380875,
+			lng: -75.69955,
+			name: "B61", 
+			building: "null"
+		});
+//
+var B62 = new Object({
+			lat: 45.380679,
+			lng: -75.69999,
+			name: "B62", 
+			building: "null"
+		});
+//
+////////This is where you need the points put in 
+var B63 = new Object({
+			lat: 45.38066,
+			lng: -75.700167,
+			name: "B63a", 
+			building: "VS"
+		});
+//
+var B63a = new Object({
+			lat: 45.380396,
+			lng: -75.700269,
+			name: "B63a", 
+			building: "VS"
+		});
+//
+var B64 = new Object({ //FIXME, there are 2 separate points for LA this and 67
+			lat: 45.380716,
+			lng: -75.699845,
+			name: "B64", 
+			building: "LA"
+		});
+//
+var B64a = new Object({ //FIXME, there are 2 separate points for LA this and 67
+			lat: 45.380468,
+			lng: -75.69963,
+			name: "B64a", 
+			building: "LA"
+		});
+//
+var B65 = new Object({ 
+			lat: 45.380547,
+			lng: -75.699888,
+			name: "B65", 
+			building: "null"
+		});
+//
+var B66 = new Object({ 
+			lat: 45.380456,
+			lng: -75.699979,
+			name: "B66", 
+			building: "SR"
+		});
+//
+var B66a = new Object({ 
+			lat: 45.380468,
+			lng: -75.700065,
+			name: "B66a", 
+			building: "HC"
+		});
+//
+var B67 = new Object({ //This should later on be B64b
+			lat: 45.38115,
+			lng: -75.698949,
+			name: "B67", 
+			building: "LA"
+		});
+//
+var B68 = new Object({ 
+			lat: 45.381225,
+			lng: -75.698386,
+			name: "B68", 
+			building: "SR"
+		});
+//
+var B68b = new Object({ 
+			lat: 45.381421,
+			lng: -75.69792,
+			name: "B68b", 
+			building: "SR"
+		});
+//
+var B69 = new Object({ 
+			lat: 45.38112,
+			lng: -75.697957,
+			name: "B69", 
+			building: "null"
+		});
+//
+var B70 = new Object({ 
+			lat: 45.380909,
+			lng: -75.698327,
+			name: "B70", 
+			building: "null"
+		});
+//
+var B71 = new Object({ 
+			lat: 45.380163,
+			lng: -75.699491,
+			name: "B71", 
+			building: "null"
+		});
+
+
+
+B0.surr = new Array(B2, B1, B6);
+B1.surr = new Array(B0, B2, B6, B7a);
+B2.surr = new Array(B0, B1, B5, B4, B7b);
+B3.surr = new Array(B13a, B12, B17, B31);		//Check this one
+B4.surr = new Array(B2, B5, B9, B7b, B11);
+B5.surr = new Array(B4, B2);
+B6.surr = new Array(B0, B1, B6a, B7d);
+B6a.surr = new Array(B14b, B7d, B8);
+B7.surr = new Array(B1, B9);
+B7a.surr = new Array (B2, B5, B0);
+B7b.surr = new Array(B4, B9);
+B7c.surr = new Array(B11, B12);
+B7d.surr = new Array(B6a, B6);		//check this also
+B7e.surr = new Array(B8, B9a);		//also this one
+B7f.surr = new Array(B10, B15);		//check
+B8.surr = new Array(B6a, B7e, B14b, B9a, B14b, B15);
+B9.surr = new Array(B1, B4, B7, B9a);
+B9a.surr = new Array(B7e, B7b, B8, B9, B10);
+B10.surr = new Array(B9a, B12, B17, B7f);
+B11.surr = new Array(B12, B4, B7c, B13);	//check
+B12.surr = new Array(B10, B13, B17, B31); //check
+B13.surr = new Array(B12, B17); //check
+B13a.surr = new Array(B3);	//check
+B14.surr = new Array(B15, B19);		//check
+B14a.surr = new Array(B16, B15);//check
+B14b.surr = new Array(B6, B6a, B8); //check
+B15.surr = new Array(B8, B14, B7f, B30, B19, B16, B20, B14a);
+B16.surr = new Array(B22, B15, B18, B14a);
+B17.surr = new Array(B10, B12, B31, B30a);
+B18.surr = new Array(B16, B24, B15, B30, B26x);
+B19.surr = new Array(B15, B16, B30, B20a, B21);
+B20.surr = new Array( B16, B15, B18);
+B20a.surr = new Array(B21, B19);
+B20b.surr = new Array(B28, B27, B29);
+B21.surr = new Array(B17, B27, B31, B19, B20b);
+B22.surr = new Array(B16, B21, B23, B28, B26xa);
+B23.surr = new Array(B25, B22);
+B24.surr = new Array(B18, B25, B54, B26xa, B32);
+B25.surr = new Array(B24, B23, B26xa, B26, B36, B37, B32, B34, B33);
+B26x.surr = new Array(B18);
+B26xa.surr =  new Array(B22, B24, B25);
+B26.surr = new Array(B25, B37);
+B26a.surr = new Array(B45, B29, B28);
+B27.surr = new Array(B21, B31, B22a, B28, B29, B41);
+B28.surr = new Array(B22, B27, B29, B31, B45, B42);
+B29.surr = new Array(B28, B45, B42, B27);
+B30.surr = new Array( B15, B19);
+B30a.surr = new Array(B17, B12);
+B31.surr = new Array(B27, B21, B17, B41, B12);
+B32.surr = new Array(B25, B34, B36);
+B33.surr = new Array(B26, B34);
+B34.surr = new Array(B36, B25, B32, B33);
+B35.surr = new Array(B36, B34, B38);
+B35a.surr = new Array(B54, B55);
+B35b.surr = new Array(B57);
+B36.surr = new Array(B25, B34, B33, B32, B35, B38, B54);
+B37.surr = new Array(B26, B25, B38, B39, B47, B46, B26xa);
+B38.surr = new Array(B36, B35, B37, B39, B57, B58);
+B39.surr = new Array(B38, B37, B37, B57);
+B39a.surr = new Array(B48, B47);
+B40.surr = new Array( B41, B27, B31);
+B40a.surr = new Array(B42, B53);
+B41.surr = new Array(B27, B31, B40, B44b);
+B42.surr = new Array(B29, B43, B53, B40a);
+B43.surr = new Array( B42, B53, B29);
+B43a.surr =  new Array(B51);
+B43b.surr = new Array(B48, B47);
+B43c.surr = new Array(B51, B64);
+B44.surr = new Array(B53, B52);
+B44a.surr = new Array(B42, B53);
+B44b.surr = new Array(B41);
+B45.surr = new Array(B29, B28, B46, B26b);
+B46.surr = new Array(B45, B47, B37);
+B47.surr = new Array(B37, B43b, B46, B48);
+B48.surr = new Array(B47, B49, B39a, B43a);
+B49.surr = new Array(B48, B50, B68b, B43c);
+B50.surr = new Array(B60, B49, B68, B58);
+B51.surr = new Array(B43a, B52, B49);
+B52.surr = new Array(B51, B53, B44);
+B53.surr = new Array(B42, B52, B40a);
+B54.surr = new Array(B55, B24, B36);
+B55.surr = new Array(B56, B54);
+B56.surr = new Array( B55, B57, B62, B63);
+B57.surr = new Array(B38, B56, B58, B59, B35b);
+B58.surr = new Array(B38, B57, B50, B68, B59a, B68);
+B59.surr = new Array(B57);
+B59a.surr = new Array(B60, B58, B61);
+B59b.surr = new Array(B57, B56);
+B60.surr = new Array(B61, B58, B50, B67, B59a, B68, B39);
+B61.surr = new Array(B60, B62, B66, B64, B67);
+B62.surr = new Array(B66, B66a, B61, B63, B64, B63, B71);
+B63.surr = new Array(B66, B61, B62);
+B63a.surr = new Array(B66, B65);
+B64.surr = new Array(B63, B61, B66);
+B64a.surr = new Array(B71, B69);
+B65.surr = new Array(B62, B71, B56, B63a, B66, B66a);
+B66.surr = new Array(B63a, B65);
+B66a.surr = new Array(B63a, B65);
+B67.surr = new Array(B60, B61);
+B68.surr = new Array(B50, B49);
+B68b.surr = new Array(B48, B49);
+B69.surr = new Array(B51, B70);
+B70.surr = new Array(B50, B69, B60, B71);
+B71.surr = new Array(B64a, B65, B70);
+
+
+
+
+
+
+
+
+
+
+
+
