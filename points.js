5a86e1d89e0931c26f972a0d0eb1b4d5c8dd7b3a
IMaz6 <ihtisham-6@hotmail.com>
2014-02-26 01:37:25 -0600

    Points.js - First set of points (A)

---
+++ points.js
@@ -0,0 +1,612 @@
+//	
+
+
+/*
+ALL COMMENTS ARE DONE BY ZACH WALTER 
+
+If you have any questions add me on facebook or mail at:
+
+zacharie.gauthier@gmail.com 
+
+*/
+
+
+// Here are the points with no polylines (points from A23d to A41)
+var A0 = new Object({// A0 
+			lat: 45.38851,
+			lng: -75.696978,
+			name: "A0",
+			building: "null"
+		});
+
+//	
+var A1 = new Object({// A1 
+			lng: -75.697729,
+			name: "A1",
+			building: "null"
+		});
+//
+var A2 = new Object({//A2 is scratched up. 
+			lat: 45.388024,
+			lng: -75.69841,
+			name: "A2",
+			building: "null"
+		});
+//
+var A3 = new Object({
+			lat: 45.388024,
+			lng: -75.697852,
+			name: "A3",
+			building: "null"
+		});
+var A4 = new Object({
+			lat: 45.388095,
+			lng: -75.697343,
+			name: "A4",
+			building: "null"
+		});
+//
+var A5 = new Object({// A5 is scratched up.
+			lat: 45.38828,
+			lng: -75.697729,
+			name: "A5",
+			building: "LE"
+		});	
+//
+var A5a = new Object({
+			lat: 45.387738,
+			lng: -75.698619,
+			name: "A5a",
+			building: "null"
+});	
+//
+var A5b = new Object({
+			lat: 45.387594,
+			lng: -75.698743,
+			name: "A5b",
+			building: "null"
+		});	
+//
+var A5c = new Object({// 
+			lat: 45.387493,
+			lng: -75.699048,
+			name: "A5c",
+			building: "LE"
+		});	
+//
+var A6 = new Object({//A6 is scratched up.
+			lat: 45.3877,
+			lng: -75.69819,
+			name: "A6",
+			building: "SP"
+		});
+//
+var A6a = new Object({//A6a has some points that look far from it.
+			lat: 45.387542,
+			lng: -75.698614,
+			name: "A6a",
+			building: "SP"
+		});	
+//
+var A6b = new Object({// A6b as no lines
+			lat: 45.387229,
+			lng: -75.698405,
+			name: "A6b",
+			building: "SP"
+		});	
+//
+var A6c = new Object({// A6c is scratched up.
+			lat: 45.387387,
+			lng: -75.698002,
+			name: "A6c",
+			building: "SP"
+		});	
+//
+var A7 = new Object({//A7 is scratched up.
+			lat: 45.387704,
+			lng: -75.697852,
+			name: "A7",
+			building: "null"
+		});
+//
+var A8 = new Object({// A8 is scratched up.
+			lat: 45.388058,
+			lng: -75.697385,
+			name: "A8",
+			building: "SD"
+		});
+//
+var A8a = new Object({
+			lat: 45.38793,
+			lng: -75.697369,
+			name: "A8a", 
+			building: "SD"
+		});
+//
+var A8b = new Object({//A8b is scratched up.
+			lat: 45.387591,
+			lng: -75.697611,
+			name: "A8b", 
+			building: "SD"
+		});
+//
+var A8c = new Object({
+			lat: 45.387621,
+			lng: -75.697316,
+			name: "A8c", 
+			building: "SD"
+		});
+//
+var A9 = new Object({
+			lat: 45.38738,
+			lng: -75.699075,
+			name: "A9", 
+			building: "null"
+		});
+//
+var A10 = new Object({// A10 is scratched up.
+			lat: 45.387553,
+			lng: -75.697852,
+			name: "A10", 
+			building: "null"
+		});
+//
+var A11 = new Object({//A11 is scratched up.
+			lat: 45.38724,
+			lng: -75.69782,
+			name: "A11", 
+			building: "null"
+		});
+//
+var A12 = new Object({
+			lat: 45.38753,
+			lng: -75.697487,
+			name: "A12", 
+			building: "null"
+		});
+//
+var A13 = new Object({// A13is scratched up.
+			lat: 45.387304,
+			lng: -75.697455,
+			name: "A13", 
+			building: "CO"
+		});
+//
+var A13a = new Object({// A13a, seams off.
+			lat: 45.387116,
+			lng: -75.697659,
+			name: "A13a", 
+			building: "CO"
+		});
+//
+var A13b = new Object({//A13b, seams off.
+			lat: 45.386909,
+			lng: -75.697482,
+			name: "A13b", 
+			building: "CO"
+		});
+//
+var A13c = new Object({//A13c lines are not showing
+			lat: 45.386928,
+			lng: -75.697176,
+			name: "A13c", 
+			building: "CO"
+		});
+//
+var A13d = new Object({
+			lat: 45.387323,
+			lng: -75.697106,
+			name: "A13d", 
+			building: "CO"
+		});
+//
+var A14 = new Object({
+			lat: 45.3875,
+			lng: -75.696929,
+			name: "A14", 
+			building: "null"
+		});
+//
+var A15 = new Object({
+			lat: 45.388088,
+			lng: -75.696763,
+			name: "A15", 
+			building: "null"
+		});
+//
+var A15a = new Object({
+			lat: 45.387576,
+			lng: -75.696554,
+			name: "A15a", 
+			building: "null"
+		});
+//
+var A15b = new Object({// A15b has a point far away
+			lat: 45.387169,
+			lng: -75.696511,
+			name: "A15b", 
+			building: "null"
+		});
+//
+var A16 = new Object({// A16 seams off
+			lat: 45.387022,
+			lng: -75.69914,
+			name: "A16", 
+			building: "null"
+		});
+//
+var A17 = new Object({// A17 seams off
+			lat: 45.387097,
+			lng: -75.698603,
+			name: "A17", 
+			building: "null"
+		});
+//
+var A18 = new Object({
+			lat: 45.387071,
+			lng: -75.697852,
+			name: "A18", 
+			building: "null"
+		});
+//
+var A19 = new Object({
+			lat: 45.386943,
+			lng: -75.698045,
+			name: "A19", 
+			building: "RU"
+		});
+//
+var A19a = new Object({
+			lat: 45.386867,
+			lng: -75.698684,
+			name: "A19a", 
+			building: "RU"
+		});
+//
+var A20 = new Object({
+			lat: 45.386863,
+			lng: -75.698544,
+			name: "A20", 
+			building: "GP"
+		});
+//
+var A20a = new Object({
+			lat: 45.386852,
+			lng: -75.698415,
+			name: "A20a", 
+			building: "GP"
+		});
+//
+var A20b = new Object({
+			lat: 45.386166,
+			lng: -75.698474,
+			name: "A20b", 
+			building: "GP"
+		});
+//
+var A21 = new Object({
+			lat: 45.386792,
+			lng: -75.698973,
+			name: "A21", 
+			building: "null"
+		});
+//
+var A21a = new Object({
+			lat: 45.386159,
+			lng: -75.698963,
+			name: "A21a", 
+			building: "null"
+		});
+//
+var A22 = new Object({
+			lat: 45.386826,
+			lng: -75.697847,
+			name: "A22", 
+			building: "null"
+		});
+//
+var A23 = new Object({
+			lat: 45.386788,
+			lng: -75.697428,
+			name: "A23", 
+			building: "GH"
+		});
+//
+var A23a = new Object({
+			lat: 45.386668,
+			lng: -75.697675,
+			name: "A23a", 
+			building: "GH"
+		});
+//
+var A23b = new Object({
+			lat: 45.386641,
+			lng: -75.697225,
+			name: "A23b", 
+			building: "GH"
+		});
+//
+var A23c = new Object({
+			lat: 45.386649,
+			lng: -75.69687,
+			name: "A23c", 
+			building: "GH"
+		});
+//
+var A23d = new Object({
+			lat: 45.386796,
+			lng: -75.697144,
+			name: "A23d", 
+			building: "GH"
+		});
+//
+var A24 = new Object({
+			lat: 45.386276,
+			lng: -75.696409,
+			name: "A24", 
+			building: "null"
+		});
+//
+var A25 = new Object({
+			lat: 45.386249,
+			lng: -75.697847,
+			name: "A25", 
+			building: "null"
+		});
+//
+var A26 = new Object({
+			lat: 45.386385,
+			lng: -75.697627,
+			name: "A26", 
+			building: "LX"
+		});
+//
+var A26a = new Object({
+			lat: 45.38637,
+			lng: -75.696822,
+			name: "A26", 
+			building: "LX"
+		});
+//
+var A26b = new Object({
+			lat: 45.386445,
+			lng: -75.696887,
+			name: "A26", 
+			building: "LX"
+		});
+//
+var A27 = new Object({
+			lat: 45.386106,
+			lng: -75.698823,
+			name: "A27", 
+			building: "FR"
+		});
+//
+var A27a = new Object({
+			lat: 45.386061,
+			lng: -75.697782,
+			name: "A27a", 
+			building: "FR"
+		});
+//
+var A28 = new Object({
+			lat: 45.386238,
+			lng: -75.697847,
+			name: "A28", 
+			building: "null"
+		});
+//
+var A29 = new Object({
+			lat: 45.386114,
+			lng: -75.697772,
+			name: "A29", 
+			building: "null"
+		});
+//
+var A30 = new Object({
+			lat: 45.386204,
+			lng: -75.696511,
+			name: "A30", 
+			building: "null"
+		});
+//
+var A31 = new Object({
+			lat: 45.38614,
+			lng: -75.697664,
+			name: "A31", 
+			building: "RH"
+		});
+//
+var A31a = new Object({
+			lat: 45.386072,
+			lng: -75.697278,
+			name: "A31a", 
+			building: "RH"
+		});
+//
+var A31b = new Object({
+			lat: 45.386133,
+			lng: -75.696742,
+			name: "A31b", 
+			building: "RH"
+		});
+//
+var A32 = new Object({
+			lat: 45.38591,
+			lng: -75.697697,
+			name: "A32", 
+			building: "null"
+		});
+//
+var A33 = new Object({
+			lat: 45.385726,
+			lng: -75.698866,
+			name: "A34", 
+			building: "LH"
+		});
+//
+var A34 = new Object({
+			lat: 45.38591,
+			lng: -75.697697,
+			name: "A34", 
+			building: "LH"
+		});
+//
+var A34b = new Object({
+			lat: 45.385812,
+			lng: -75.698651,
+			name: "A34b", 
+			building: "LH"
+		});
+//
+var A35 = new Object({
+			lat: 45.385737,
+			lng: -75.697643,
+			name: "A35", 
+			building: "null"
+		});
+//
+var A36 = new Object({
+			lat: 45.385989,
+			lng: -75.696393,
+			name: "A36", 
+			building: "null"
+		});
+//
+var A38 = new Object({
+			lat: 45.385632,
+			lng: -75.69718,
+			name: "A38", 
+			building: "PH"
+		});
+//
+var A38a = new Object({
+			lat: 45.38594,
+			lng: -75.696527,
+			name: "A38a", 
+			building: "PH"
+		});
+//
+var A38b = new Object({
+			lat: 45.385933,
+			lng: -75.696758,
+			name: "A38b", 
+			building: "PH"
+		});
+//
+var A39 = new Object({
+			lat: 45.385375,
+			lng: -75.698592,
+			name: "A39", 
+			building: "null"
+		});
+//
+var A40 = new Object({
+			lat: 45.38556,
+			lng:-75.697659,
+			name: "A40", 
+			building: "null"
+		});
+//
+var A41 = new Object({
+			lat: 45.385571,
+			lng: -75.696382,
+			name: "A41", 
+			building: "null"
+		});
+A0.surr = new Array(A1, A15); 
+A1.surr = new Array(A2, A3, A4);
+A2.surr = new Array(A5, A5a, A6, A3, A1, A7);
+A3.surr = new Array(A1, A2, A4, A5, A5a, A6, A7);
+A4.surr = new Array(A0, A1, A3, A8);
+A5.surr = new Array(A2, A5a, A5b, A6);
+A5a.surr = new Array(A2, A3, A5, A5b, A6, A7 );//Is reaching to A1 for some reason
+A5b.surr = new Array(A5, A5c, A6a, A9); //Added A5 
+A5c.surr = new Array(A9, A5b,A16);//Added A16
+A6.surr = new Array(A2, A3, A5, A5a, A7);//Removed A5b it was redundant
+A6a.surr = new Array( A5b, A9, A16);// Rmovd A5a also Reaches  A3 for some reason
+A6b.surr = new Array(A16, A17, A18);//Nothing shows up.
+A6c.surr = new Array(A10, A11, A12, A13);//Is wrong but point
+A7.surr = new Array(A3, A6, A10);//Points are logical but looks messy
+A8.surr = new Array(A4, A8a);//Logical but looks wrong
+A8a.surr = new Array(A8, A12);
+A8b.surr = new Array(A10, A11, A12, A13);//Logical but looks wrong
+A8c.surr = new Array(A12, A13);//Logical but looks wrong
+A9.surr = new Array(A5c, A5b, A6a, A16, A17);
+A10.surr = new Array(A6c, A7, A8b, A11, A12,);//Rmovd A13 redundant because A12
+A11.surr = new Array(A6c,A10, A12, A13a, A17, A18);//Added A6c
+A12.surr = new Array(A6c, A8b, A8c, A10, A11, A13);//Rmovd A8a,because of A8b
+A13.surr = new Array(A10, A11,A12);// Added A11 
+A13a.surr = new Array(A11, A18, A22);
+A13b.surr = new Array(A13c, A22);
+A13c.surr = new Array(A13b, A24);
+A13d.surr = new Array(A13, A14);
+A14.surr = new Array(A13d, A8c, A15a);
+A15.surr = new Array(A0, A1, A4, A15a);
+A15a.surr = new Array(A14, A15, A15b);
+A15b.surr = new Array(A15a, A24);//Should add a point connecting with the building.
+A16.surr = new Array(A5b, A9, A17, A21);
+A17.surr = new Array(A6b, A9, A11, A16, A18, A19); //Rmovd A5b because of A9
+A18.surr = new Array(A11, A17, A13a, A22, A19);
+A19.surr = new Array(A17, A18, A22);
+A19a.surr = new Array(A21);// Rmovd A17 it makes it go across th building
+A20.surr = new Array(A21, A20a);// Rmovd A27 becaus it is far
+A20a.surr = new Array(A20, A25, A22);
+A20b.surr = new Array(A27, A28, A25);//Replaced A22 by A28 //NO POLYLINES
+A21.surr = new Array(A16, A19a, A20, A27);
+A21a.surr = new Array(A21, A27, A33);//Rmovd A28 it is far 
+A22.surr = new Array(A13b, A18, A19, A20a, A23, A25);
+A23.surr = new Array(A22, A18);
+A23a.surr = new Array(A22, A25);
+A23b.surr = new Array(); //nothing 
+A23c.surr = new Array(A24, A26b, A30);//Added A26b because it is close //NO POLYLINES
+A23d.surr = new Array(A24); //NO POLYLINES
+A24.surr = new Array(A15b, A30,A23c, A26b);//Removed A25 too far, Replaced A22c by A23c because it doesn't exist and Replaced A26c for the same reason//NO POLYLINES
+A25.surr = new Array(A19, A20a, A22, A23a, A24, A26, A28);//NO POLYLINES
+A26.surr = new Array(A25, A22,A28);//Added A28 //NO POLYLINES
+A26a.surr = new Array(A30, A36); //NO POLYLINES
+A26b.surr = new Array(A24, A30); //NO POLYLINES
+A27.surr = new Array(A20b,A21, A21a);//Added A20b//NO POLYLINES
+A27a.surr = new Array(A28, A29,A32)//Added A32;//NO POLYLINES
+A28.surr = new Array(A25, A26, A27a, A29);//Rmovd A30 it is far //NO POLYLINES
+A29.surr = new Array(A28, A32, A27a); //NO POLYLINES
+A30.surr = new Array(A24, A26a, A36);//Rmovd A28 it is far//NO POLYLINES
+A31.surr = new Array(A28, A29); //NO POLYLINES
+A31a.surr = new Array();//nothing
+A31b.surr = new Arrray(A36, A30); //NO POLYLINES
+A32.surr = new Array(A29, A31,A34a A35, A27a);//Removed A36 and A33 they are far. Added A34a//NO POLYLINES
+A33.surr = new Array(A21a, A34, A27, A39);//NO POLYLINES
+A34.surr = new Array(A21, A33,A39);//Added A39 //NO POLYLINES
+A34a.surr = new Array(A28, A29, A32); //NO POLYLINESr
+A35.surr = new Array(A32, A34a, A40, A38); //NO POLYLINES
+A36.surr = new Array(A31b, A32, A38a, A41);//Removed A20 it is far
+A38.surr = new Array(A40, A41);
+A38a.surr = new Array(A36, A41);
+A38b.surr = new Array(A32,A29);//Added A29
+A39.surr = new Array(A33, A40); //from OTHER SECTION REQUIRED
+A40.surr = new Array(A39, A41, A35) //NO POLYLINES
+A41.surr = new Array(A26, A40, A38); //NO POLYLINES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
